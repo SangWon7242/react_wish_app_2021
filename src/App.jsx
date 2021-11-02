16f97c24c2391390e7a6f85e8e490525aaf7dbbf
SangWon7242 <gotkdqja@gmail.com>
2021-11-03 02:10:37 +0900

    데이지 UI 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="bg-red-500 h-[100px] text-white">앱</div>;
+  return (
+    <div className="bg-red-500 h-[100px] text-white">
+      <button className="btn btn-primary">버튼</button>
+    </div>
+  );
 }
 
 export default App;
